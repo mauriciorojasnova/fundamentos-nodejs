c5185a5387a8b5e647f12a1c4a3808d3a3fe24a3
mauriciorojasnova <andrsnov@gmail.com>
2021-08-17 22:34:11 -0500

    Añadí información sobre error first

---
+++ trucos/errorFirst.js
@@ -0,0 +1,23 @@
+function asincrona(callback) {
+    setTimeout(function() {
+        try {
+            let a = 3 + z;
+            callback(null, a)
+        } catch (e) {
+            callback(e);
+        }
+    }, 1000)
+}
+
+
+    asincrona(function (err, dato) {
+        if (err) {
+            console.error('Tenemos un error');
+            console.error(err);
+            return false;
+            //throw err; //No va a funcionar
+            
+        }
+    
+        console.log('Todo ha ido bien, mi data es: ', dato);
+    })
